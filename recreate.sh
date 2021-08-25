addfd1275d779e86800b394bcc26d4414d04c971
Luguoba <80254980+luguoba@users.noreply.github.com>
2021-08-25 08:17:06 +0800

    sw

--- recreate.sh
+++ recreate.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-who
+var1=
+
+function find {
+    for file in $1/*; do
+        if [ -d "$file" ]; then
+            find $file
+            echo "$file is a directory"
+        elif [ -f "$file" ]; then
+            #var1+=$file
+            echo "$file is a file"
+        fi
+    done
+}
+
+find .
+
